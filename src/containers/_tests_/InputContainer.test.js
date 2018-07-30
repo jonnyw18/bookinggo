f1f24d4880c5451cd07941b17b29eb47c5f475b6
Jonathan <jonny_ward@hotmail.com>
2018-07-30 23:33:08 +0100

    added basic renders correctly test to InputContainer

---
+++ src/containers/_tests_/InputContainer.test.js
@@ -0,0 +1,8 @@
+import InputContainer from "../InputContainer";
+
+describe('<InputContainer />', () => {
+    it('renders correctly', () => {
+        const wrapper = shallow(<InputContainer />);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
